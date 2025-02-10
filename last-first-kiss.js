763b160e74da7eed080efbc3c32c0f8e520d0083
chmyint <bigk89865@gmail.com>
2025-02-10 16:33:14 +0200

    sixth commit

---
+++ last-first-kiss.js
@@ -0,0 +1,13 @@
+function first (aors){
+    return aors[0];
+}
+
+function last (aors){
+    return aors[aors.length - 1];
+}
+
+function kiss (aors){
+    return [aors[aors.length - 1], aors[0]];
+}
+
+
