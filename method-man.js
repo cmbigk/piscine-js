a18f5f0de7094bec20d4017565d2ed3bee686108
cmbigk <bigk89865@gmail.com>
2025-02-10 21:24:24 +0200

    okay

---
+++ method-man.js
@@ -0,0 +1,20 @@
+function words (str) {
+    return str.split(' ');
+    }
+    
+    function sentence (str) {
+    return str.join(' ');
+    }
+
+    function yell(str) {
+        return str.toUpperCase();
+
+    }
+
+    function whisper(str) {
+        return "*str.toLowerCase()*";
+    }
+
+    function capitalize(str) {
+        return str.spilt(' '). str[0].toUpperCase() + str.slice(1).toLowerCase().join(' ');
+    }
