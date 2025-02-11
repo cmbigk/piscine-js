2a349acb714e786ad14d546bb3a0b79bf30786b6
chmyint <bigk89865@gmail.com>
2025-02-11 12:49:27 +0200

    okk

--- physics.js
+++ physics.js
@@ -1,7 +1,16 @@
 function getAcceleration(f,m) {
+
     if (m<=0 || f<=0) {
+        return "impossible";
+    }
+
+    if (arguments.length<2) {
         return "impossible";
     }
     return f/m;
 
 }
+
+
+console.log(getAcceleration(10, 5));
+console.log(getAcceleration());
