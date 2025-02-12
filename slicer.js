efc5455e33ab6a36cfb708072624b84a0212069e
chmyint <bigk89865@gmail.com>
2025-02-12 13:34:50 +0200

    leebl

--- slicer.js
+++ slicer.js
@@ -1,4 +1,4 @@
-function slice(array, start, end) {
+function slicer(array, start, end) {
     if (start < 0) start = array.length + start;
     if (end === undefined) end = array.length; 
     if (end < 0) end = array.length + end; 
@@ -9,6 +9,3 @@ function slice(array, start, end) {
     return sliced;   
 }
 
-console.log(slice(arr[1, 2, 3, 4, 5], 1, 3)); // [2, 3]
-console.log(slice([1,2,3,4,5,6,7,8,9],-2))
-console.log(slice([1,2,3,4,5,6,7,8,9],-3,-1))
