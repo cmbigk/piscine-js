4916dd623518c0a5e5301858f6b3eefa5aedaa18
chmyint <bigk89865@gmail.com>
2025-02-12 12:03:40 +0200

    correction

--- index-of.js
+++ index-of.js
@@ -1,5 +1,5 @@
-function indexOf(collection, target){
-    for (let i = 0; i < collection.length; i++){
+function indexOf(collection, target,index){
+    for (let i = index; i < collection.length; i++){
         if (collection[i] === target){
             return i;
         }
@@ -7,8 +7,8 @@ function indexOf(collection, target){
     return -1;
 }
 
-function lastIndexOf(collection, target){
-    for (let i = collection.length - 1; i >= 0; i--){
+function lastIndexOf(collection, target,index){
+    for (let i = collection.length - 1; i >= index; i--){
         if (collection[i] === target){
             return i;
         }
@@ -16,6 +16,6 @@ function lastIndexOf(collection, target){
     return -1;
 }   
 
-function includes(collection, target){
-    return indexOf(collection, target) ;
+function includes(collection, target,index){
+    return indexOf(collection, target,index)!== -1;     
 }
