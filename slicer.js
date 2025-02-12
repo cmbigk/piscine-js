bbf8e83c6a1d39869da9c4788e8cebadbf0288c3
chmyint <bigk89865@gmail.com>
2025-02-12 13:42:14 +0200

    leeb l

--- slicer.js
+++ slicer.js
@@ -1,4 +1,4 @@
-function slicer(array, start, end) {
+function slice(array, start, end) {
     if (start < 0) start = array.length + start;
     if (end === undefined) end = array.length; 
     if (end < 0) end = array.length + end; 
