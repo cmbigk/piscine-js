7d47dda1f96a40c50bc6f7b949e482fb26002d68
chmyint <bigk89865@gmail.com>
2025-02-12 14:15:20 +0200

    stupid

--- index-of.js
+++ index-of.js
@@ -1,11 +1,14 @@
 function indexOf(arr, item, index) {
-    for (var i = index || 0; i < arr.length; i++) {
+    for (let i = index || 0; i < arr.length; i++) {
         if (arr[i] === item) {
             return i;
         }
     }
     return -1;
 }
+
+const arr = [1, 2, 3, 4, 5 , 4];
+console.log(indexOf(arr, 4)); // 3
 
 function lastIndexOf(arr, item, index) {
     for (var i = index || arr.length - 1; i >= 0; i--) {
--- slicer.js
+++ slicer.js
@@ -1,11 +1,16 @@
 function slice(array, start, end) {
+    if (typeof array === 'string') {
+        array = [...array]; 
+    }
+
     if (start < 0) start = array.length + start;
     if (end === undefined) end = array.length; 
     if (end < 0) end = array.length + end; 
+    
     var sliced = [];
     for (var i = start; i < end; i++) {
         sliced.push(array[i]);
     }
-    return sliced;   
+    
+    return typeof array === 'string' ? sliced.join('') : sliced;  
 }
-
