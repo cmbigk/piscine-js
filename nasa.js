fde0f87263b45252162013f12b02a976fc81b6a3
chmyint <bigk89865@gmail.com>
2025-02-13 17:31:07 +0200

    okk

--- nasa.js
+++ nasa.js
@@ -1,19 +1,21 @@
-function nasa (N) {
-  let result = []
-  for (let i = 1; i <= N; i++) {
-    if (i % 3 === 0 && i % 5 === 0) {
-      result.push('NASA')
-    } else if (i % 3 === 0) {
-      result.push('N')
-    } else if (i % 5 === 0) {
-      result.push('S')
-    } else {
-      result.push(i)
+function nasa(int) {
+    var result = "";
+    for (var i = 1; i <= int; i++) {
+        if (i % 3 === 0 && i % 5 === 0) {
+            result = result + "NASA";
+        } else if (i % 3 === 0) {
+            result = result + "NA";
+        } else if (i % 5 === 0) {
+            result = result + "SA";
+        } else {
+            result = result + i.toString();
+        }
+        if (i !== int) {
+            result = result + " ";
+        }
     }
-  }
-  return result
+    return result;
 }
-
 
 
 console.log(15)
