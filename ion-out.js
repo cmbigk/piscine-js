298fffc50c4ebcc357037199a735edd76baa9647
chmyint <bigk89865@gmail.com>
2025-02-14 09:31:08 +0200

    just changing file loc.

--- ion-out.js
+++ ion-out.js
@@ -10,6 +10,7 @@ function ionOut(str) {
     return res;
 }
 
+/*
 console.log(ionOut('attention, direction'), ['attent', 'direct']); // Should print true
 console.log(ionOut('promotion, provision'), ['promot']); // Should print true
 console.log(ionOut('transfusion'), []); // Should print true
@@ -17,3 +18,4 @@ console.log(
   ionOut(' 1st position is the vision of the 2nd position'),
   ['posit', 'posit']
 ); // Should print true
+*/
