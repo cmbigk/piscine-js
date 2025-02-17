19ee26d7e37728cd98203047bfce9cb5d1160f84
chmyint <bigk89865@gmail.com>
2025-02-17 11:12:39 +0200

    okkkk

---
+++ dr-strange.js
@@ -0,0 +1,44 @@
+const customWeekdays = [
+    "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday",
+    "secondMonday", "secondTuesday", "secondWednesday", "secondThursday",
+    "secondFriday", "secondSaturday", "secondSunday"
+  ];
+  
+  function addWeek(newDate) {
+    const defaultDate = new Date('0001-01-01');
+    
+
+    const datesDifference = Math.floor((newDate - defaultDate) / 86400000);
+
+    const weekdayIndex = datesDifference % 14;
+  
+    return customWeekdays[weekdayIndex];
+  }
+  
+  console.log(addWeek(new Date('0001-01-09'))) 
+  console.log(addWeek(new Date('0001-01-02'))) 
+  console.log(addWeek(new Date('0001-01-01')))
+  console.log(addWeek(new Date('0001-01-07'))) 
+  console.log(addWeek(new Date('0001-01-08'))) 
+  
+
+
+  function timeTravel({date,hour,minute,second})
+    {
+        const newDate = new Date(date);
+        newDate.setHours(hour);
+        newDate.setMinutes(minute);
+        newDate.setSeconds(second);
+        return newDate;
+    }
+
+
+    console.log(
+        timeTravel({
+          date: new Date('2024-02-17 10:15:45'),
+          hour: 5,
+          minute: 30,
+          second: 0,
+        }).toString()
+      );
+      
