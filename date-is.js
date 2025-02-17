c14a93396a5f9541cbbf22ae15225d25005141b8
chmyint <bigk89865@gmail.com>
2025-02-17 11:33:12 +0200

    okkk

--- date-is.js
+++ date-is.js
@@ -1,8 +1,6 @@
 function isValid(date) {
-    return date instanceof Date && !isNaN(date.getTime());
+    return (date instanceof Date && !isNaN(date.getTime())) || (typeof date === 'number' && !isNaN(date));
   }
-
-
 function isAfter(date1, date2) {
   return date1 > date2;
 }
@@ -18,7 +16,7 @@ function isFuture(date) {
 function isPast(date) {
     return isValid(date) < new Date();
 }
-
+/*
 console.log(isValid(new Date())); // true
 console.log(isValid(new Date('Invalid Date'))); // false
 console.log(isValid(Date.now())); // false (since it's a number)
@@ -28,3 +26,5 @@ console.log(isBefore(new Date('2023-01-01'), new Date('2023-12-01'))); // true
 
 console.log(isFuture(new Date('2077-11-31'))); // true
 console.log(isPast(new Date('1442-11-31'))); // true
+
+*/
