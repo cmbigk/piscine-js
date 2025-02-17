4899599cbdc2a9cad62a39608fd48390e71ee9f9
chmyint <bigk89865@gmail.com>
2025-02-17 16:29:19 +0200

    okay

--- sunny-sunday.js
+++ sunny-sunday.js
@@ -1,4 +1,4 @@
-function bloodySunday(date) {
+function sunnySunday(date) {
     let counter = -1;
     const week = [
         "Monday",
@@ -16,3 +16,19 @@ function bloodySunday(date) {
     console.log(week[counter % 6]);
     return week[counter % 6];
 }
+
+
+
+console.log(sunnySunday(new Date('0001-01-01')) === 'Monday')
+console.log(sunnySunday(new Date('0001-01-02')) === 'Tuesday')
+console.log(sunnySunday(new Date('0001-01-03')) === 'Wednesday')
+console.log(sunnySunday(new Date('0001-01-04')) === 'Thursday')
+console.log(sunnySunday(new Date('0001-01-05')) === 'Friday')
+console.log(sunnySunday(new Date('0001-01-06')) === 'Saturday')
+console.log(sunnySunday(new Date('0001-01-07')) === 'Monday')
+
+console.log(sunnySunday(new Date('0001-12-01')) === 'Friday')
+console.log(sunnySunday(new Date('1664-08-09')) === 'Saturday')
+
+console.log(sunnySunday(new Date('2020-01-01')) === 'Monday')
+console.log(sunnySunday(new Date('2048-12-08')) === 'Thursday')
