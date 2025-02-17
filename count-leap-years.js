be58f0b21d01439c84af7a957a520d3a7b3e0b6f
chmyint <bigk89865@gmail.com>
2025-02-17 15:04:06 +0200

    corrected code

--- count-leap-years.js
+++ count-leap-years.js
@@ -1,14 +1,10 @@
-function countLeapYears(date){
-    let yearcount =0;
-    for(let i = 0; i <= date.getFullYear(); i++){
-        if(i % 4 === 0 &&(i%100 !== 0 || i % 400 === 0)){
-            yearcount++;
-        }
-    }
-    return yearcount;
+function countLeapYears(date) {
+    let year = date.getFullYear();
+    return Math.floor(year / 4) - Math.floor(year / 100) + Math.floor(year / 400);
 }
 
-console.log(countLeapYears(new Date('0001-12-01')) === 0)
-console.log(countLeapYears(new Date('1664-08-09')) === 403)
-console.log(countLeapYears(new Date('2020-01-01')) === 489)
-console.log(countLeapYears(new Date('2048-12-08')) === 496)
+// Test cases
+console.log(countLeapYears(new Date('0001-12-01')) === 0);
+console.log(countLeapYears(new Date('1664-08-09')) === 403);
+console.log(countLeapYears(new Date('2020-01-01')) === 489);
+console.log(countLeapYears(new Date('2048-12-08')) === 496);
