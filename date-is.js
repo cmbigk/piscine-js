e15e1dca6a63fa6583da7ba8e26a4be01ede0e3a
chmyint <bigk89865@gmail.com>
2025-02-17 11:36:38 +0200

    okk

--- date-is.js
+++ date-is.js
@@ -10,13 +10,14 @@ function isBefore(date1, date2) {
     }
 
 function isFuture(date) {
-    return isValid(date) > new Date();
+    return isValid(date)&& date > new Date();
 }   
 
 function isPast(date) {
-    return isValid(date) < new Date();
+    return isValid(date) && date < new Date();
 }
-/*
+
+
 console.log(isValid(new Date())); // true
 console.log(isValid(new Date('Invalid Date'))); // false
 console.log(isValid(Date.now())); // false (since it's a number)
@@ -24,7 +25,6 @@ console.log(isValid(Date.now())); // false (since it's a number)
 console.log(!isAfter(new Date('2024-02-17'), new Date('2023-12-01'))); // true
 console.log(isBefore(new Date('2023-01-01'), new Date('2023-12-01'))); // true
 
-console.log(isFuture(new Date('2077-11-31'))); // true
+console.log(isFuture(new Date(2077-11-31))); // true
 console.log(isPast(new Date('1442-11-31'))); // true
 
-*/
