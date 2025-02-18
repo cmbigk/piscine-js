cd88da75dc1b7cd66ab1290c532a04079a9d6004
chmyint <bigk89865@gmail.com>
2025-02-18 17:02:12 +0200

    corrected

--- using-reduce.js
+++ using-reduce.js
@@ -1,25 +1,32 @@
-function adder(arr){
-    return arr.reduce((acc,curr) => acc + curr)
+function adder(arr, value) {
+    return arr.reduce(
+        (acc, item) => acc + item,
+        value === undefined ? 0 : value
+    );
 }
 
-function sumOrMul(arr){
-    return arr.reduce((acc,curr) => {
-        if(curr%2 === 0){
-            return acc * curr
-        }else{
-            return acc + curr
-        }
-    })
-}   
+function sumOrMul(arr, value) {
+    return arr.reduce(
+        (acc, item) => {
+            if (item % 2 === 0) {
+                return acc * item;
+            } else {
+                return acc + item;
+            }
+        },
+        value === undefined ? 0 : value
+    );
+}
 
-function funcExec(arr,value){
-    return arr.reduce((acc,curr) => {
-        if (typeof item === 'function') {
-            return item(acc,value);
-        }else{
-            return acc;
-        }
-    },
-    value === undefined ? 0 : value
-);
+function funcExec(arr, value) {
+    return arr.reduce(
+        (acc, item) => {
+            if (typeof item === "function") {
+                return item(acc, value);
+            } else {
+                return acc;
+            }
+        },
+        value === undefined ? 0 : value
+    );
 }
